5288bc289ad1934047ecf6ae941ced4ff758e0f1
tthisag246 <tthisag246@gmail.com>
2024-05-19 09:34:56 +0900

    cors 오류 해결

--- src/main/java/com/example/moo/controller/ProductReviewController.java
+++ src/main/java/com/example/moo/controller/ProductReviewController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "*", allowedHeaders = "*")
+@CrossOrigin(allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/product-review")
 public class ProductReviewController {
